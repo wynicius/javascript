e52f767424bedac003ec36dff9f5bc40838238ad
wynicius <wynicius@gmail.com>
2022-09-16 16:21:51 -0300

    Desenvolvimento de uma página de conversão de real para dólar
    HTML5 CSS3 e JAVASCRIPT

--- imersao-dev-alura/functions.js
+++ imersao-dev-alura/functions.js
@@ -1,8 +1,13 @@
 function Converter() {
-    var valorElemento = document.getElementById("valor");
-    var valor = valorElemento.value;
-    var valorEmDolarNumerico = parseFloat(valor);
-
-    var valorEmReal = valorEmDolarNumerico * 5;
-    console.log(valorEmReal);
+  var valorElemento = document.getElementById("valor"); // valor é o id do <input>
+  var valor = valorElemento.value; // aqui se usa o .value para que retorne apenar o valor inserido no input, que vem sempre em texto POR PADRÃO!
+  var valorEmReal = parseFloat(valor); // transformação do valor de string para float
+  
+  var valorEmDolar = valorEmReal * 5.26 // a conversão em dolar
+    console.log(valorEmDolar);
+  
+  var elementoValorConvertido = document.getElementById("valorConvertido");
+  var valorConvertido = "o valor em dolar é $ " + valorEmDolar;
+  elementoValorConvertido.innerHTML = valorConvertido;
+  event.preventDefault()
 }
