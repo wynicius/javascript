183e3b7c1190371eaad46c2e4b4e0a705e94ea9d
wynicius <wynicius@gmail.com>
2022-09-16 17:56:34 -0300

    Mudança no JS

--- imersao-dev-alura/conversor-real-dolar/function.js
+++ imersao-dev-alura/conversor-real-dolar/function.js
@@ -3,10 +3,11 @@ function Converter() {
   var valor = valorElemento.value; // aqui se usa o .value para que retorne apenar o valor inserido no input, que vem sempre em texto POR PADRÃO!
   var valorEmReal = parseFloat(valor); // transformação do valor de string para float
   
-  var valorEmDolar = valorEmReal * 5.26 // a conversão em dolar
+  var valorEmDolar = (valorEmReal * 5.26).toFixed(2) // a conversão em dolar
+
   
   var elementoValorConvertido = document.getElementById("valorConvertido");
-  var valorConvertido = "O valor em dólar é $ " + valorEmDolar;
+  var valorConvertido = "O valor em Real é de R$" + valorEmDolar;
   elementoValorConvertido.innerHTML = valorConvertido;
   event.preventDefault()
 }
