d395bd4df2f99fbe06c65cf9b5264a1b768cc65e
wynicius <wynicius@gmail.com>
2022-09-16 09:12:51 -0300

    Exercício de cálculo de média

---
+++ calculo-de-media.js
@@ -0,0 +1,11 @@
+var nome = "guilherme"
+var GQ1 = 9.51
+var GQ2 = 5.84
+
+
+var MReg = (GQ1*2+GQ2*3)/5
+if (MReg > 7) {
+    console.log("Sua média ficou em " + MReg, "e você está aprovado!")   
+} else {
+    console.log("Sua média ficou em " + MReg, "e você está na final")
+}
