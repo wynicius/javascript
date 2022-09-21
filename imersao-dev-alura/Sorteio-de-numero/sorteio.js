f197d8b696eea9ccb54a376c090a7babd89921e5
wynicius <wynicius@gmail.com>
2022-09-21 12:16:02 -0300

    Update sorteio.js

--- imersao-dev-alura/Sorteio-de-numero/sorteio.js
+++ imersao-dev-alura/Sorteio-de-numero/sorteio.js
@@ -4,7 +4,7 @@ function Chutar() {
     var chute = parseInt(document.getElementById("valor").value)
     var msgResultado = document.getElementById("msgResultado")
     if (chute == numeroSortido) {
-        msgResultado.innerHTML = "Você Acertou e ganhou U$1.000.000!!!"
+        msgResultado.innerHTML = "Você acertou e ganhou U$1.000.000!!!"
     } else if (chute>10 || chute<0) {
         msgResultado.innerHTML = "Opps... Você digitou um número não permitido!"
     } else {
