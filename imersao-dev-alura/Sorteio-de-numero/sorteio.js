a6041399e26c9d12b88a73e527630f9e62798bf5
wynicius <wynicius@gmail.com>
2022-09-21 11:24:20 -0300

    Exercício de If, else is, else em JS

---
+++ imersao-dev-alura/Sorteio-de-numero/sorteio.js
@@ -0,0 +1,13 @@
+var numeroSortido = parseInt(Math.random() * 11)
+
+function Chutar() {
+    var chute = parseInt(document.getElementById("valor").value)
+    var msgResultado = document.getElementById("msgResultado")
+    if (chute == numeroSortido) {
+        msgResultado.innerHTML = "Você Acertou e ganhou U$1.000.000!!!"
+    } else if (chute>10 || chute<0) {
+        msgResultado.innerHTML = "Opps... Você digitou um número não permitido!"
+    } else {
+        msgResultado.innerHTML = "Você errou, o número certo era " + numeroSortido + "... mas vamos lá, tente novamente!"
+    }
+}
--- imersao-dev-alura/conversor-real-dolar/function.js
+++ imersao-dev-alura/conversor-real-dolar/function.js
@@ -1,6 +1,9 @@
 function Converter() {
-  var valorElemento = document.getElementById("valor"); // valor é o id do <input>
-  var valor = valorElemento.value; // aqui se usa o .value para que retorne apenar o valor inserido no input, que vem sempre em texto POR PADRÃO!
+  /* 
+  * var valorElemento = document.getElementById("valor"); // valor é o id do <input>
+  * var valor = valorElemento.value; // aqui se usa o .value para que retorne apenar o valor inserido no input, que vem sempre em texto POR PADRÃO!
+  FAREI A TROCA DAS DUAS LINHAS POR APENAS UMA!*/
+  var valor = document.getElementById("valor").value
   var valorEmReal = parseFloat(valor); // transformação do valor de string para float
   
   var valorEmDolar = (valorEmReal * 5.26).toFixed(2) // a conversão em dolar
