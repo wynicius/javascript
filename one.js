26f5a79f79da78e0fc6103500a93ab4c83befd84
wynicius <wynicius@gmail.com>
2022-11-08 16:50:27 -0300

    Create one.js

---
+++ one.js
@@ -0,0 +1,8 @@
+function ultimaParada(combustivel,consumo,postosDeGasolina) {
+    let combustivel = 2;
+    let consumo = 8
+    let postosDeGasolina = [
+        2,15,22,10.2
+    ]
+    const kmsFinais = 
+}
