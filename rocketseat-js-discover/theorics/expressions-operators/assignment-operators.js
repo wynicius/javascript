dfafe07877be937eaf2d7a960e19ab66c2609c60
Wynícius M. da Silva <wynicius@gmail.com>
2022-12-04 19:17:48 -0300

    more notes on it

--- rocketseat-js-discover/theorics/expressions-operators/assignment-operators.js
+++ rocketseat-js-discover/theorics/expressions-operators/assignment-operators.js
@@ -1,4 +1,33 @@
 // Operadores de Atribuição
 let x
 
+// DICA: subir e descer o console.log() para verificar o resultado das operações
+
+// assignment
+x = 1
+
+// addition assignment
+// isto é igual à expressão x = x + 2
+x += 2
+
+
+// subtraction assignment
+// isto é igual à expressão x = x - 1
+x -= 1
+
+// multiplication assignment
+// x = x * 2
+x *= 2
+
+// division assignment 
+// x = x / 2
+x /= 2
 console.log(x)
+
+// remainder
+// x = x % 2
+x %= 2
+
+// exponentiation
+// x = x ** 2
+x **= 2
