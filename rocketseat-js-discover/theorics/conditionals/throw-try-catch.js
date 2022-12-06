42e1ecf8fd2ad6e6704ee58f9c46d93f947be131
Wynícius M. da Silva <wynicius@gmail.com>
2022-12-06 15:15:08 -0300

    Studies on conditional structures
    more notes and exercises

---
+++ rocketseat-js-discover/theorics/conditionals/throw-try-catch.js
@@ -0,0 +1,25 @@
+// Nesta aula veremos sobre o throw e try/catch. São parte do controle de fluxo da aplicação.
+
+// Isso significa que vamos tentar executar um bloco de código, e se der algum erro, será disparado e capturado na nossa aplicação. Suponhamos que haja uma função que dispare um erro caso não seja passado um parâmetro dessa função.
+
+function sayMyName(name = '') {
+    if (name === '') {
+        throw 'Nome é obrigatório'
+    }
+
+    console.log(name)
+}
+
+// Nesse caso, se o nome vier vazio, será disparada uma mensagem 'Nome é obrigatório'
+
+// Precisamos agora usar o try/catch para capturarmos esse erro, caso contrário, ele irá encerrar nossa aplicação, e nós o faremos da seguinte maneira:
+
+try {
+    sayMyName()
+} catch(e) {
+    console.log(e)
+}
+
+console.log('após ao try/catch, provando que a API continuou')
+
+// O try irá executar a função, enquanto o catch vai capturar o erro e atribuir à variável e, que só existe dentro do catch. Após isso, mesmo com o erro, nossa aplicação não será interrompida por completo.
