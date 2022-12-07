92562e652b2392b6291e7d09343b832ef5beb172
wynicius <wynicius@gmail.com>
2022-12-07 18:49:37 -0300

    Coded a temperature converter

--- rocketseat-js-discover/exercises/temperature-convertion/temperature-convertion.js
+++ rocketseat-js-discover/exercises/temperature-convertion/temperature-convertion.js
@@ -2,10 +2,27 @@
 //     C = (F - 32) * 5/9 
 //     F = C * 9/5 + 32
 
-let celsius
-let fahrenheit
-convertion()
+let celsius, fahrenheit
 
-function convertion() {
-    
+function CtoF() {
+   celsius = document.getElementById("tempC").value;
+   let tempC = parseFloat(celsius);
+   let tempF = (tempC * 9/5 + 32).toFixed(0)
+   var elementToBeShown = document.getElementById("celsiusToF");
+
+   var tempConverted = "The temperature right now in fahrenheit is " + tempF + '°F';
+   elementToBeShown.innerHTML = tempConverted;
+   event.preventDefault()
 }
+
+function FtoC() {
+   fahrenheit = document.getElementById("tempF").value;
+   let tempF = parseFloat(fahrenheit);
+   let tempC = ((tempF - 32) * 5/9).toFixed(0)
+   var elementToBeShown = document.getElementById("fahrenheitToC");
+
+   var tempConverted = "The temperature right now in celsius is " + tempC + '°C';
+   elementToBeShown.innerHTML = tempConverted;
+   event.preventDefault()
+}
+
