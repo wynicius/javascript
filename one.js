c58e9a4558024f33f9651b9fe7cb71a9edc1c175
Wynícius M. da Silva <wynicius@gmail.com>
2022-12-12 02:47:49 -0300

    Deleted garbage

--- one.js
+++
@@ -1,8 +0,0 @@
-function ultimaParada(combustivel,consumo,postosDeGasolina) {
-    let combustivel = 2;
-    let consumo = 8
-    let postosDeGasolina = [
-        2,15,22,10.2
-    ]
-    const kmsFinais = 
-}
